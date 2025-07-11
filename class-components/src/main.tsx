8dad99bf25369f2742a2aad84fad0f6f4f695916
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-07-11 10:15:50 +0500

    feat: add basic page layout

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
+import './index.css';
+import App from './App.tsx';
+
+const container = document.getElementById('root');
+
+if (!container) throw new Error('Root element not found');
+
+createRoot(container).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
