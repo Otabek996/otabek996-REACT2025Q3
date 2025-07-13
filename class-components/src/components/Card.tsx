54ee2580045326c64f46e14f23bddb549d228249
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-07-13 18:15:12 +0500

    feat: add card style

--- class-components/src/components/Card.tsx
+++ class-components/src/components/Card.tsx
@@ -10,8 +10,15 @@ class Card extends Component<Props> {
     const { character } = this.props;
 
     return (
-      <div className="card">
-        {character.name} - {character.species}
+      <div className="card bg-[#1E1E1E] rounded-2xl m-4">
+        <img className="card-img rounded-t-2xl" src={character.image} />
+        <div className="p-3">
+          <h2 className="card-name text-xl font-medium">{character.name}</h2>
+          <h6 className="card-species text-base font-normal text-[#ffffffb3]">
+            {character.species}
+          </h6>
+          <p className="card-status">{character.status}</p>
+        </div>
       </div>
     );
   }
