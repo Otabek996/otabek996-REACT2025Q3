be649a133ead81ff457ec49a0cdb0ea3da49ee8f
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-07-14 02:30:36 +0500

    refactor: apply gh-pages

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/otabek996-REACT2025Q3/',
   plugins: [react(), tailwindcss()],
 });
