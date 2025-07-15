9293cd198f920a785fb5bdf051e0ee0b61d5c27d
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-07-15 13:23:41 +0500

    init: start unit testing task

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -6,4 +7,10 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   base: '/otabek996-REACT2025Q3/',
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/__test__/setupTests.ts',
+    css: true,
+  },
 });
