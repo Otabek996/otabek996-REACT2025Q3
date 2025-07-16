4607798be1eaef101f9e9f67e5f08188d6b46224
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-07-16 14:58:32 +0500

    feat: exclude main.tsx file from tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -12,5 +13,30 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/__test__/setupTests.ts',
     css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+        'src/types/**',
+        'node_modules/**',
+        'src/main.tsx',
+      ],
+      all: true,
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
   },
 });
