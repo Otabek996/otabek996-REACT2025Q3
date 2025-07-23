3e59c4aeb1f21622cfd488236b7c0e7e2715c662
Tuychiev Otabek <otabekotabek996996@gmail.com>
2025-07-23 16:45:10 +0500

    feat: add application author information

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -2,6 +2,14 @@ function AboutPage() {
   return (
     <section className="about-page">
       <h1>This is ABOUT Page</h1>
+      <p className="py-2">Application author - It is me :)</p>
+      <a
+        href="https://github.com/Otabek996"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Otabek
+      </a>
     </section>
   );
 }
